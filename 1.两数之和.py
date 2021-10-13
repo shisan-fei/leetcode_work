b6a663e0f5ab2b6db0aaf2c96492029d2b2d4270
shisan-fei <1340673657@qq.com>
2021-10-13 18:19:47 +0800

    两数之和

--- 1.两数之和.py
+++ 1.两数之和.py
@@ -5,9 +5,22 @@
 #
 
 # @lc code=start
+from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
+        hash={}     #创建一个字典用来存放列表中数字和数字所在索引
+        j=-1        #j用来返回数字所在索引
+        for ind,num in enumerate(nums):   #enumerate返回可迭代对象的值和索引('索引'，'值')
+            hash[num] = ind               #字典的键为列表的数字，值为数字的索引
+        # print(hash)           
+        for ind,num in enumerate(nums):   
+            j=hash.get(target-num)        #target-num就是和减去列表中任意一个数字，结果在字典中找，找到后将索引返回给j
+            if j != None and  ind !=j:    #target-num的结果找不到j就会返回none
+                return [ind,j]
+            
 # @lc code=end
-
+nums = [2,5,3,7,9]
+test = Solution()
+num =test.twoSum(nums,8)
+print(num)
 
